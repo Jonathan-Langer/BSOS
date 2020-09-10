8576e0a13fa31942a653cfdf9f2dcd7b58f2097b
dror <User@DESKTOP-1UJ2P8K>
2020-09-10 14:42:43 +0300

    Adding Orders Class

---
+++ Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BSOS.Models
+{
+    public class Orders
+    {
+        public int OrderID { get; set; }
+        [Display(Name ="Order Date")]
+        public DateTime OrderDate { get; set; }
+        [Display(Name ="Selected Product")]
+        public Product* SelectedProduct { get; set; }
+        public Client client { get; set; }
+    }
+}
