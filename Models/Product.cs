5b6f94388029b54bba3055214b23d173e5333c97
Jonathan <jona.lan03@cs.colman.ac.il>
2020-09-10 14:49:30 +0300

    New Class Product

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BSOS.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Display(Name="Name")]
+        public string ProductName { get; set; }
+        public int Size { get; set; }
+        public string Manufacturer { get; set; }
+        public string Importer { get; set; }
+        public string Color { get; set; }
+    }
+}
