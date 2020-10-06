de75febc5aed1281ab9eab9149499267b049cc73
eden <edenbarhum@gmail.com>
2020-10-06 13:36:48 +0300

    eden

--- Startup.cs
+++ Startup.cs
@@ -58,7 +58,7 @@ namespace BSOS
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
